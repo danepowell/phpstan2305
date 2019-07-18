b7e673e478177a32e94b63ffc1857840dd1423f6
Dane Powell <git@danepowell.com>
2019-07-18 12:18:42 -0700

    fixed script

--- phpstan2305.sh
+++ phpstan2305.sh
@@ -3,5 +3,4 @@
 git clone git@github.com:danepowell/phpstan2305.git
 cd phpstan2305
 composer install
-wget https://raw.githubusercontent.com/acquia/orca/develop/src/Task/DeprecatedCodeScanner/phpstan.neon
 ./vendor/bin/phpstan analyse --configuration=phpstan.neon vendor/acquia/blt/src/Composer
